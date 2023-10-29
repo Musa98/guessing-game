6f71ed8f0550aad60781038aa8efb47f7273da88
Musa Mamedov <gsunnymus@gmail.com>
2023-10-30 01:20:59 +0300

    master: create guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.guessNumber = Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
